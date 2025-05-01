9fd680f4f7299251220a5695e246f1feaffcc151
Chobby12 <git add omenyitk@gmail.com>
2025-05-01 16:53:47 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+name = "Ozichukwu"
+age = 39
+phone = 8074794
+isMarried = False
+isGreaterThanTen = 2 > 10
+isLessThanTwo = 3 < 2
+print("is Greater Than 10:", isGreaterThanTen)
+print("Is Less that 2:", isLessThanTwo)
+True and False
+
+#  Equals to !=
+print(2==2)
+print("!=")
+print(2!=2)
+ObiLiesTooMuch = True
+print(ObiLiesTooMuch)
+print(">=")
+print("<=")
+print(4!=4)
+flag_color = 'red'
+print('What is the color of the flag?', flag_color)
+
+height = 3
+
+if (height>10):
+    print("Very Tall!")
+elif (height <= 10 and height>=5):
+    print("Tall!")
+else:
+    print("Short!") 
+
+#  Data types: Integer, Boolean, Float, String 
+#  Conditional Statement: if, elif and else
+
+for i in range(100):
+    print(i+1, "I am sorry, Ma!")
+
+odd_numbers = [i for i in range(10) if i%2 != 0]
+print(odd_numbers)
+
+# Modulo
+print(23%10)
+
+if 4 % 2 == 0 : 
+    print("Even")
+else:
+    print("Odd")
+
+for number in range(20):
+    print(number+1, "cry!")
---
+++ index.js
@@ -0,0 +1,11 @@
+const height = 3
+
+result = height > 10 ? "Tall" : "Short"
+
+if(height < 10){
+    console.log("Very Tall!");
+}else if(height <= 10 && height>=5){
+    console.log("Tall!");
+}else{
+    console.log("Short!")
+}
